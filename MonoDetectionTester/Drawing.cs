44ebdfe7a7af846809b60769727ab82daf164e11
TrumpReborn <73669113+TrumpReborn@users.noreply.github.com>
2020-11-21 20:31:03 +0000

    added files which were missing for some reason

---
+++ MonoDetectionTester/Drawing.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MonoDetectionTester 
+{
+    public class Drawing : MonoBehaviour
+    {
+
+        public void OnGUI()
+        {
+            GUI.Label(new Rect(1000, 200, 1000, 1000), "Testing");
+        
+        
+        
+        }
+
+
+
+    }
+}
---
+++ MonoDetectionTester/loader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+
+namespace MonoDetectionTester
+{
+    public class loader 
+    { 
+        public static void Initialize()
+        {
+            
+            GO.AddComponent<Drawing>();
+            UnityEngine.Object.DontDestroyOnLoad(loader.GO);
+            
+        }
+        public static GameObject GO = new GameObject();
+     
+    }
+  
+}
